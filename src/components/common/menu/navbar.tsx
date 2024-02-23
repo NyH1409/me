d879c340c04f046d33a6f5236bffd2b6789ebb85
NyH1409 <hei.nyhasina@gmail.com>
2024-02-23 19:38:49 +0300

    feat: navigation bar

---
+++ src/components/common/menu/navbar.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react"
+
+
+export const NavBar : FC = () => {
+
+    return(
+        <>
+        
+        </>
+    )
+}
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,10 @@
+import { NavBar } from "@/components/common/menu";
+import { FC } from "react";
+
+export const Home : FC = ()=>{
+    return (
+        <div className="w-screen h-screen">
+            <NavBar />
+        </div>
+    )
+}
