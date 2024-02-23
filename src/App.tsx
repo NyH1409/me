a2d476f67fb21e365df43e065465f145cf20f0b9
NyH1409 <hei.nyhasina@gmail.com>
2024-02-23 18:57:59 +0300

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-center'>
+        Welcome to Picksary!
+        Stay tunned, features are comming soon!
+    </div>
+  )
+}
+
+export default App
