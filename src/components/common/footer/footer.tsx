5c04e217313d0271233b3c2c1be0714bb122740e
NyH1409 <hei.nyhasina@gmail.com>
2024-03-15 09:31:58 +0300

    chore: responsive footer

--- src/components/common/footer/footer.tsx
+++ src/components/common/footer/footer.tsx
@@ -5,7 +5,25 @@ import { Icons } from "../icons/icons";
 export const Footer: FC = ()=>{
     return(
         <div id="footer" className="absolute z-24 b-0 w-full">
-            <div className="flex my-1">
+            <div className="md:flex my-1">
+                <div className="px-20 md:pt-20 py-20 bg-blue-400" style={{ flexBasis: "50%"}}>
+                    <form action="" method="post">
+                        <div className="form">
+                            <input type="text" className="w-[90%] rounded-[4px] p-2 text-xs" placeholder="example@gmail.com" name="" id="" />
+                        </div>
+                        <div className="md:flex w-[90%] my-4 justify-between">
+                            <div className="form">
+                                <input type="text" className="rounded-[4px] w-full md:w-[100%] p-2 text-xs" placeholder="example@gmail.com" name="" id="" />
+                            </div>
+                            <div className="form">
+                                <input type="text" className="rounded-[4px] md:mt-0 mt-2 w-full md:w-[100%] p-2 text-xs" placeholder="example@gmail.com" name="" id="" />
+                            </div>
+                        </div>
+                        <div className="form my-2">
+                            <textarea name="" id="" className="w-[90%] h-[200px] p-2 rounded-[4px] text-xs" placeholder="Write here..."></textarea>
+                        </div>
+                    </form>
+                </div>
                 <div className="bg-gray-200 p-20 text-justufy text-xs" style={{ flexBasis: "50%"}}>
                     <h1 className="font-bold text-2xl">Get a website that will make a lasting impression.</h1>
                     <br />
@@ -20,9 +38,6 @@ export const Footer: FC = ()=>{
                     </div>
                     <br />
                     <p> &copy; Copyright 2024, powered by Ny Hasina VAGNO</p>
-                </div>
-                <div className="p-20 bg-blue-400" style={{ flexBasis: "50%"}}>
-                    
                 </div>
             </div>
         </div>
